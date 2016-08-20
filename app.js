3f210d29b854e0e8d3a1127c7562398ff946d2ef
Darrell Abrau <darrellabrau@gmail.com>
2016-08-20 13:35:04 -0700

    create config.json

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var restify = require('restify');
-var server = restify.createServer();
-server.use(restify.bodyParser());
+var config = require('./config');
+var app = restify.createServer();
 
+app.use(restify.bodyParser());
+app.listen(config.port, () => {
+  console.log('server listening on port number', config.port)
+})
 var mongoose = require('mongoose');
 //conect to mongodb
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(config.dbPath);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
   console.log('successfuly opened the db')
 });
 
