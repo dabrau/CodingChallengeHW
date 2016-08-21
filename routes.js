c9a96d92d9b247a033682fa46bb659a62f7be7b4
Darrell Abrau <darrellabrau@gmail.com>
2016-08-21 15:05:57 -0700

    add routes

--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
 module.exports = function(app) {
+  var scoreboard = require('./controllers/scoreboardController')
+
   app.get('/', function(req, res, next) {
     return res.send("WELCOME TO REST API");
   });
+
+  app.post('/scoreboard', scoreboard.createScoreboard);
+  app.get('/scoreboard', scoreboard.getScoreboard);
 };
