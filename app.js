fcb8380738120c582b88b1afdd0baa07b3568c79
Darrell Abrau <darrellabrau@gmail.com>
2016-08-22 20:25:48 -0700

    require db in app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var restify = require('restify');
 var config = require('./config');
+
+var restify = require('restify');
 var app = restify.createServer();
-require('./db.js')
 
 app.use(restify.bodyParser());
 app.listen(config.port, () => {
   console.log('server listening on port number', config.port)
 });
 
-
-var routes = require('./routes')(app);
+require('./db.js')(config.dbPath)
+require('./routes')(app);
--- db.js
+++ db.js
@@ -1,20 +1,21 @@
-var mongoose = require('mongoose');
-var config = require('./config');
+module.exports = (path) => { 
+  var mongoose = require('mongoose');
 
-//connect to db
-mongoose.connect(config.dbPath);
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-  console.log('successfuly opened the db');
-});
+  //connect to db
+  mongoose.connect(path);
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', () => {
+    console.log('successfuly opened the db');
+  });
 
-//register models
-var exported_model, i,  path_fn, 
-  files = ['participantSchema.js', 'scoreboardSchema.js'];
+  //register models
+  var exported_model, i,  path_fn, 
+    files = ['participantSchema.js', 'scoreboardSchema.js'];
 
-for(i = 0; i < files.length; i++) {
-  path_fn = "./models/" + files[i];
-  exported_model = require(path_fn);
+  for(i = 0; i < files.length; i++) {
+    path_fn = "./models/" + files[i];
+    exported_model = require(path_fn);
+  }
 }
 
